4488c2789c69fab9a05a9e114f74f2c5c9800372
Ashmit Jain <ashmit27j@gmail.com>
2023-10-22 23:22:53 +0530

    Updated UI for home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,20 +8,19 @@ export default function Home() {
 					width={0}
 					height={0}
 					sizes="100vw"
-					className="p-10 min-w-[17rem] md:min-w-[22rem] lg:min-w-[23rem] bg-transparent"
+					className=" p-7 min-w-[17rem] ml-16 mt-10 md:min-w-[22rem] lg:min-w-[23rem] bg-transparent"
 					alt="Homepage Image"
 				/>
 			</div>
-			<section className="min-h-[90vh] max-h-[100vh] min-w-full flex flex-col lg:flex-row justify-center lg:justify-between items-center p-14 gap-20 lg:gap-0 text-center lg:text-left">
+			<section className="min-h-[90vh] max-h-[100vh] min-w-full flex flex-col lg:flex-row justify-center lg:justify-between items-center p-20 gap-20 lg:gap-0 text-center lg:text-left">
 				{" "}
 				<div>
 					<h1 className="font-bold text-5xl lg:text-7xl tracking-wider bg-transparent">
-						Welcome to{" "}
+						Welcome to{" "}<br className="hidden lg:block"></br>
 						<span className=" font-mono bg-transparent ">
 							<span className=" text-[#F06] bg-transparent">
 								Code
 							</span>
-							<br className=" hidden lg:block"></br>
 							<span className=" bg-transparent text-cyan-600">
 								Connect{" "}
 							</span>{" "}
@@ -35,7 +34,7 @@ export default function Home() {
 						type="button"
 						className=" text-black bg-white hover:opacity-90 border-4 border-t-0 border-l-0 border-b-[#F06] border-r-[#F06]  focus:outline-none font-medium rounded-lg text-md  md:text-lg px-7 py-3.5  text-center inline-flex items-center mr-2 mb-2 "
 					>
-						Start Here
+						Start Here!
 					</button>
 				</div>
 				<Image
@@ -43,7 +42,7 @@ export default function Home() {
 					width={0}
 					height={0}
 					sizes="100vw"
-					className=" w-fit max-w-[20rem]  lg:w-[32rem] lg:max-w-[35rem] bg-transparent order-first lg:order-last"
+					className=" w-fit max-w-[20rem]  lg:w-[40rem] lg:max-w-[40rem] bg-transparent order-first lg:order-last"
 					alt="Homepage Image"
 				/>
 			</section>
